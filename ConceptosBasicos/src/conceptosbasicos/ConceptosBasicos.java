d9371d31fea380d24735ffa290b0f679f9d2698d
Aram <aramreal43@aragon.unam.mx>
2021-03-22 13:22:08 -0600

    Subiendo

--- ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -18,13 +18,27 @@ public class ConceptosBasicos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Bocho
         Automovil miBocho = new Automovil();
         miBocho.setMarca("VW");
         miBocho.setSubMarca("Sedan");
         miBocho.setModelo(1970);
         miBocho.setColor(Color.BLUE);
         System.out.println( miBocho);
+        //Akura
+        Automovil miAkura = new Automovil();
+        miAkura.setMarca("Akura");
+        miAkura.setSubMarca("NSX");
+        miAkura.setModelo(2013);
+        miAkura.setColor(Color.GRAY);
+        System.out.println( miAkura);
+        //Mustang
+        Automovil miMustang = new Automovil();
+        miMustang.setMarca("Ford");
+        miMustang.setSubMarca("Mustang");
+        miMustang.setModelo(2010);
+        miMustang.setColor(Color.yellow);
+        System.out.println( miMustang);
     }
     
 }
